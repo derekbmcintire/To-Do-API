8f5c516b2c1fef79e973b995ff1ae75e48856ad6
derekbmcintire <derekbmcintire@gmail.com>
2017-12-28 11:26:45 -0500

    Change app title

--- app/controllers/root.js
+++ app/controllers/root.js
@@ -6,7 +6,7 @@ const controller = require('lib/wiring/controller')
 const root = (req, res) => {
   res.json({
     index: {
-      title: 'Express Template',
+      title: 'To Do List API',
       environment: req.app.get('env')
     }
   })
